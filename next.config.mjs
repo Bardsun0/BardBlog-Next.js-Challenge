c3cef59bf0d687b013b15c8aa5a6ebc803b1044a
Ozan Güneş <ozangunes2894@gmail.com>
2025-01-15 20:01:53 +0300

    Bug Fixed!

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['localhost'], // Eğer resimler localhost'tan yükleniyorsa
+    },
+  };
+  
+  export default nextConfig;
